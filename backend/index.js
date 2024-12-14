a8a1b4b11a7bb6d564b585cd75896c53ca232b61
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2024-12-14 11:46:05 +0530

    DB Schema, models and routes

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,15 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+//app.use('/api/auth',require('./routes/auth'))
+/*app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+})*/
+
+
+//Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port,()=>{
     console.log(`listening at http://localhost:${port}`);
