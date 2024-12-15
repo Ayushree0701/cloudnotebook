cbb47efe530448d15418b59c655b107394962775
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2024-12-15 17:55:27 +0530

    Simple validation using express validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult} = require('express-validator');
 
 //create user using POST "/api/auth/", does not require auth
-router.post('/', (req, res)=>{
+router.post('/', [
+    body('name').isLength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({min : 5})
+],
+    (req, res)=>{
     console.log(req.body);
-    const user = User(req.body);
-    user.save();
+   
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     res.send(req.body);
 })
 
